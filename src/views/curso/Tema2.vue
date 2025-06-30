7a4ba05c4a2f1cc043c924ea93556726ba5880c1
Francisco Lizcano <flizcanor@icloud.com>
2025-06-30 09:51:49 -0500

    Avances 3

--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -95,7 +95,7 @@
       .p-4.p-md-5
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/9cqLwqWDCzs?si=F1b2M6SlLRnGqFf9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/qs4sw-gp9yI?si=7pR0yvP-L187UGNd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     h2.mb-5(data-aos="fade-left") El papel del estudiante
 
